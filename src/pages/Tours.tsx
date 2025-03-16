4e616c51f7209cd48f84c12c78fa0956041f5d6c
Nguyễn Nghĩa <128125577+Nguyenghia@users.noreply.github.com>
2025-03-16 20:31:14 +0700

    version 1.2.3

--- src/pages/Tours.tsx
+++ src/pages/Tours.tsx
@@ -71,7 +71,7 @@ const Tours = () => {
     },
     {
       id: 'mui-ne',
-      name: 'Mũi né',
+      name: 'Mũi Né',
       image: 'https://images.unsplash.com/photo-1716479852874-22742b84fef5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGhhbiUyMHRoaSVFMSVCQSVCRnR8ZW58MHx8MHx8fDA%3D',
       location: 'Mũi Né',
       duration: '2 ngày 1 đêm',
